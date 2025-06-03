c2cdfe8aba97b71f8abaf5dc5d61a763a7d0b9ae
Daivd093 <david.tapiap@sansano.usm.cl>
2025-06-03 01:35:49 -0400

    Interpretar .tflite

--- HelloWorld_Models/model_info.py
+++ HelloWorld_Models/model_info.py
@@ -1,9 +1,16 @@
 import tensorflow as tf
 
 
-model_path = "sine_model_perfect_int8.tflite"
+model_path = "HelloWorld_Models\sine_model_perfect_int8.tflite" # Esto es suponiendo que se corre desde la base del repositorio
 
 interprete = tf.lite.Interpreter(model_path=model_path)
 interprete.allocate_tensors()
 
+input_details = interprete.get_input_details()
+output_details = interprete.get_output_details()
+tensor_details = interprete.get_tensor_details()
+
+print("input_details = ", input_details ,'\n\n')
+print("output_details = ", output_details, '\n\n' )
+print("tensor_details = ", tensor_details, '\n\n' )
 
